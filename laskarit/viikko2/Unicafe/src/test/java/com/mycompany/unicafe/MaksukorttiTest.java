4cb64f65dd52f1e53a09221e86060d2cf0d0498d
Jeemlei <eemeli.ingervo@gmail.com>
2020-03-22 02:40:27 +0200

    vk2 teht2

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -1,5 +1,6 @@
 package com.mycompany.unicafe;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,5 +17,39 @@ public class MaksukorttiTest {
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void saldoKonstruktorillaOikein() {
+        assertThat(kortti.toString(), is("saldo: 0.10"));
+    }
+    
+    @Test
+    public void lataaRahaaToimii() {
+        kortti.lataaRahaa(1);
+        assertThat(kortti.toString(), is("saldo: 0.11"));
+    }
+    
+    @Test
+    public void saldoaOnTarpeeksi() {
+        kortti.otaRahaa(1);
+        assertThat(kortti.toString(), is("saldo: 0.9"));
+    }
+    
+    @Test
+    public void saldoaEiTarpeeksi() {
+        kortti.otaRahaa(11);
+        assertThat(kortti.toString(), is("saldo: 0.10"));
+    }
+    
+    @Test
+    public void saldoaOnTarpeeksiTrue() {
+        assertThat(kortti.otaRahaa(1), is(true));
+    }
+    
+    @Test
+    public void saldoaEiTarpeeksiFalse() {
+        
+        assertThat(kortti.otaRahaa(11), is(false));
     }
 }
