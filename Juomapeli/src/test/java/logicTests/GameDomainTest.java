e852f55e7d433bc6d498758f0009bf966f31d6ab
Jeemlei <eemeli.ingervo@gmail.com>
2020-03-30 23:39:27 +0300

    Ensimmäiset testit

---
+++ Juomapeli/src/test/java/logicTests/GameDomainTest.java
@@ -0,0 +1,70 @@
+package logicTests;
+
+import juomapeli.GameDomain;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Eemeli
+ */
+public class GameDomainTest {
+
+    GameDomain game;
+
+    public GameDomainTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        this.game = new GameDomain();
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void addPlayer() {
+        this.game.addPlayer("a");
+        int playerCount = this.game.getPlayerCount();
+        assertThat(playerCount, is(1));
+    }
+
+    @Test
+    public void addTooManyPlayers() {
+        for (int i = 0; i < 9; i++) {
+            this.game.addPlayer(Integer.toString(i));
+        }
+        int playerCount = this.game.getPlayerCount();
+        assertThat(playerCount, is(8));
+    }
+
+    @Test
+    public void addNamelessPlayer() {
+        this.game.addPlayer("");
+        int playerCount = this.game.getPlayerCount();
+        assertThat(playerCount, is(0));
+    }
+
+    @Test
+    public void addPlayerAlreadyInGame() {
+        this.game.addPlayer("a");
+        this.game.addPlayer("a");
+        int playerCount = this.game.getPlayerCount();
+        assertThat(playerCount, is(1));
+    }
+}
